d2c1f30223367aeb407631d0484dabbf8b9e36b7
Jeka <jekagalkin@gmail.com>
2022-05-26 18:01:09 +0300

    added disable button func and fixed bug with onclick handler

--- newfile.js
+++
@@ -1,8 +0,0 @@
-function sum(n) {
-    return function arg2(m) {
-        return n+m;
-    }
-}
-
-let a = 1;
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,40 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { addAbortSignal } = require('stream');
+const { randomFill } = require('crypto');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/scripts.js',
-    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: './scripts.js',
     output: {
-        filename: '[name].[contenthash].js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
+            template: './index.html',
+            favicon: './img/favicon.ico'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        port: 8800
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpg|ttf|ico)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            }
+        ],
+    },
+  
 }
