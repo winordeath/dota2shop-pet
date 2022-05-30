1b4720cb4efa16c2a22b2baf990e2cd719352801
Jeka <jekagalkin@gmail.com>
2022-05-30 20:03:48 +0300

    made promisses for gamble data and card data

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
 import createCard from "./create-card";
 
-// const divData = document.getElementById("data");
-// const dataItemsJson = divData.dataset.items;
-// const dataArr = JSON.parse(dataItemsJson);
-const divDataGamble = document.getElementById("data-gamble");
-const divDataGambleJson = divDataGamble.dataset.items;
-const dataGambleArr = JSON.parse(divDataGambleJson);
 const cardList = document.getElementsByClassName("card");
 const nameList = document.getElementsByClassName("card__name");
 const imgList = document.getElementsByClassName("card__img");
@@ -20,26 +14,127 @@ const totalItemsElement = document.getElementById("total-items");
 const totalPriceElement = document.getElementById("total-price");
 const modalElement = document.getElementById("modal");
 const gambleElement = document.getElementById("gamble");
-let gambleTimeout;
-const requestURL = "https://6291e2709d159855f081b5ea.mockapi.io/dota2"
+const cardDataURL = "https://6291e2709d159855f081b5ea.mockapi.io/dota2";
+const gambleDataURL = "https://6291e2709d159855f081b5ea.mockapi.io/dota2gamble"
 let dataArr;
 
-function sendRequest(method, url, body = null) {
+function getData(method, url, body = null) {
     return fetch(url).then(response => {
         return response.json()
     })
 }
 
-sendRequest('GET', requestURL)
-    .then(items => {
-        renderCards(items)
-        return items
-    })
-    .catch(() => containerMain.innerHTML = `Гейб еще не завёз шмотки`)
-    .then(items => {
-        dataArr = items;
-    })
+function showInsideModal() {
+    loadGamble()
+    getData('GET', gambleDataURL)
+        .then(function (gambleData) {
+            showGambleInfo(gambleData)
+        })
+        .catch(function (reject) {
+            const modalBody = document.getElementById("modal-body");
+            modalBody.innerHTML = `
+    <div id="gamble-result">
+        <p>Gabe eshe ne zavez shmotki</p>
+        <button id="play-again-btn">PLAY AGAIN
+        </button>
+    </div>`
+        })
+}
 
+function createModal() {
+    modalElement.insertAdjacentHTML(`afterbegin`,
+        `<div id="modal-wrapper">
+        <div class="modal-block">
+            <div class="modal-header" title="Exit">
+                <p id="modal__close-button">&#10006
+                </p>
+            </div>
+            <div id="modal-body">
+            </div>
+        </div>
+    </div>`)
+    showInsideModal()
+}
+
+const validateData = (data) =>
+    data.map((item) => ({
+        ...item,
+        rate: parseFloat(item.rate.replace(',', '.')),
+
+    }))
+        .sort((a, b) => b.rate - a.rate);
+
+const generateRandomInteger = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+
+function twistRoulette(data) {
+    const validData = validateData(data);
+    const winNum = generateRandomInteger(0, 99).toFixed(2);
+    let prevScore = 0;
+
+    for (let index = 0; index < validData.length; index++) {
+        const item = validData[index];
+        const currentScore = item.rate + prevScore;
+        const isWinner = winNum <= currentScore;
+        const rarity = item.rarity;
+        const fullItemNames = {
+            "com": "Common",
+            "unc": "Uncommon",
+            "rar": "Rare",
+            "arc": "Arcana",
+            "myt": "Mythical",
+            "imm": "Immortal"
+        }
+
+        if (isWinner) {
+            return `Congratulations! You win ${fullItemNames[rarity]} item!`;
+        } else {
+            prevScore += item.rate;
+        }
+    }
+
+    return 'Better luck next time!';
+};
+
+function gamble() {
+    loadGamble();
+    gambleTimeout = setTimeout(showGambleInfo, 5000);
+}
+
+function loadGamble() {
+    const modalBody = document.getElementById("modal-body");
+
+    modalBody.innerHTML = `
+    <div id="gamble-gif">
+        <img src="https://i.gifer.com/8iwS.gif"></img>
+        </div><br>
+        <p id="gamble-text">Please wait , Gaben is twisting the roulette
+    </p>`;
+};
+
+function showGambleInfo(data) {
+    const modalBody = document.getElementById("modal-body");
+
+    modalBody.innerHTML = `
+    <div id="gamble-result">
+        <p>${twistRoulette(data)}</p>
+        <button id="play-again-btn">PLAY AGAIN
+        </button>
+    </div>`
+};
+
+function getCardData() {
+    getData('GET', cardDataURL)
+        .then(items => {
+            renderCards(items)
+            return items
+        })
+        .catch(() => containerMain.innerHTML = `Гейб еще не завёз шмотки`)
+        .then(items => {
+            dataArr = items;
+        })
+}
+
+getCardData();
 
 function renderCards(dataArr) {
 
@@ -153,93 +248,11 @@ function toFixedPrice(element) {
     }
 }
 
-const validateData = (data) =>
-    data
-        .map((item) => ({
-            ...item,
-            rate: parseFloat(item.rate.replace(',', '.')),
-        }))
-        .sort((a, b) => b.rate - a.rate);
-
-const generateRandomInteger = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
-
-function twistRoulette(data) {
-    const validData = validateData(data);
-    const winNum = generateRandomInteger(0, 99).toFixed(2);
-    let prevScore = 0;
-
-    for (let index = 0; index < validData.length; index++) {
-        const item = validData[index];
-        const currentScore = item.rate + prevScore;
-        const isWinner = winNum <= currentScore;
-        const rarity = item.rarity;
-        const fullItemNames = {
-            "com": "Common",
-            "unc": "Uncommon",
-            "rar": "Rare",
-            "arc": "Arcana",
-            "myt": "Mythical",
-            "imm": "Immortal"
-        }
-
-        if (isWinner) {
-            return `Congratulations! You win ${fullItemNames[rarity]} item!`;
-        } else {
-            prevScore += item.rate;
-        }
-    }
-
-    return 'Better luck next time!';
-};
-
-function createModal() {
-    modalElement.insertAdjacentHTML(`afterbegin`,
-        `<div id="modal-wrapper">
-        <div class="modal-block">
-            <div class="modal-header" title="Exit">
-                <p id="modal__close-button">&#10006
-                </p>
-            </div>
-            <div id="modal-body">
-            </div>
-        </div>
-    </div>`)
-    gamble();
-}
-
-function gamble() {
-    loadGamble();
-    gambleTimeout = setTimeout(showGambleInfo, 5000);
-}
-
-function loadGamble() {
-    const modalBody = document.getElementById("modal-body");
-
-    modalBody.innerHTML = `
-    <div id="gamble-gif">
-        <img src="https://i.gifer.com/8iwS.gif"></img>
-        </div><br>
-        <p id="gamble-text">Please wait , Gaben is twisting the roulette
-    </p>`;
-};
-
-function showGambleInfo() {
-    const modalBody = document.getElementById("modal-body");
-
-    modalBody.innerHTML = `
-    <div id="gamble-result">
-        <p>${twistRoulette(dataGambleArr)}</p>
-        <button id="play-again-btn">PLAY AGAIN
-        </button>
-    </div>`
-};
-
 modalElement.addEventListener(`click`, function (e) {
     const playAgainButton = e.target;
 
     if (playAgainButton.closest("#play-again-btn")) {
-        clearTimeout(gambleTimeout)
-        gamble();
+        showInsideModal()
     }
 })
 
@@ -249,7 +262,6 @@ modalElement.addEventListener(`click`, function (e) {
     if (closeButton.closest("#modal__close-button")) {
         const modalWrapper = document.getElementById("modal-wrapper");
         modalWrapper.remove();
-        clearTimeout(gambleTimeout);
     }
 })
 
